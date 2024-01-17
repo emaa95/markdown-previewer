c7f8a24026302bc0a3526d15fe347e1cfbba1f27
emaa95 <chiletti.ema@gmail.com>
2024-01-17 17:19:03 -0300

    the app works correctly

---
+++ markdown-previewer/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import { marked } from 'marked'
+import './App.css'
+
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... **_both!_**
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+`;
+
+
+function App() {
+  const [markdownText, setMarkdownText] = useState(defaultMarkdown)
+
+  marked.setOptions({
+    breaks: true
+  })
+
+  return (
+    <>
+      <div>
+        <div className='boxes-container'>
+          <textarea
+            name='editor'
+            id='editor'
+            value={markdownText}
+            onChange={(e) => setMarkdownText(e.target.value)}
+            style= {{ backgroundColor: "#c0d8d8"}}
+          >
+          </textarea>
+          <div id="preview"
+            dangerouslySetInnerHTML={{
+  __html: marked(markdownText),
+ }}
+          >
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
